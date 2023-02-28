e8c19ebbf64be514686ad2421eb13603f0e63795
JRH3221 <ts.dog3221@gmail.com>
2023-02-28 19:22:25 +1100

    Update

--- Compiler/parse.cpp
+++ Compiler/parse.cpp
@@ -2,12 +2,43 @@
 #include <string>
 #include <iostream>
 #include <fstream>
+#include <vector>
+
+void createFile() {
+	std::ofstream pyfile ("main.py");
+
+	pyfile << "testing text";
+
+	pyfile.close();
+}
 
 void compiler::Parse(std::string location) {
-	std::ifstream sr("D:/github/Repo/R-Script/Example Scripts/Test1.rscript");
+	std::ifstream sr(location);
+
+	char character;
+	std::string point;
 
 	if (sr.is_open()) {
-		std::cout << "Valid Location";
+		std::cout << "Compiling...";
+
+		std::vector<std::string> syntax = { "print" };
+
+		createFile();
+
+		while (sr) {
+			while (true) {
+				if (sr) {
+					character = sr.get();
+					if (character == ' ') break;
+					point.push_back(character);
+				}
+				else {
+					break;
+				}
+			}
+			std::cout << point;
+		}
+		std::cout << "end of file";
 	}
 	else {
 		std::cout << "Invalid Location" << std::endl;
